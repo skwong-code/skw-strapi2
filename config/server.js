445870311a1b8b9e038ba282da9ffcbb0bd3006d
Stephan Wong <80276544+skwong-code@users.noreply.github.com>
2021-11-18 16:48:24 -0700

    delete
    delete

--- config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'f7ca0d87487af4b2658e657775354ea2'),
-    },
-  },
-});
