e7de3c995d3192dc5c718b7667a9f75f9aedac1e
Stephan Wong <80276544+skwong-code@users.noreply.github.com>
2021-11-22 23:00:00 -0700

    aws-sw
    {
    
          "provider": "aws-s3",
          "providerOptions": {
            "accessKeyId": "AKIA35KCLFHBP3JCOI5F",
            "secretAccessKey": "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",
            "region": "us-west-1",
            "params": {
              "Bucket": "image-b354"
    
            }
        }
    }

--- config/plugins.js
+++ config/plugins.js
@@ -1,15 +1,15 @@
 module.exports = ({ env }) => ({
-    // ...
-    upload: {
-      provider: 'aws-s3',
-      providerOptions: {
-        accessKeyId: env('AKIA35KCLFHBP3JCOI5F'),
-        secretAccessKey: env('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'),
-        region: env('us-west-1'),
-        params: {
-          Bucket: env('image-b354'),
-        },
+  // ...
+  upload: {
+    provider: 'aws-s3',
+    providerOptions: {
+      accessKeyId: env('AWS_ACCESS_KEY_ID'),
+      secretAccessKey: env('AWS_ACCESS_SECRET'),
+      region: env('AWS_REGION'),
+      params: {
+        Bucket: env('AWS_BUCKET'),
       },
     },
-    // ...
-  });
+  },
+  // ...
+});
