4d5c1276345b59e9b7abdf04b9a481cd45ebd520
Stephan Wong <80276544+skwong-code@users.noreply.github.com>
2021-11-22 23:27:03 -0700

    update
    update

--- config/plugins.js
+++ config/plugins.js
@@ -1,15 +1,16 @@
 module.exports = ({ env }) => ({
-    // ...
-    upload: {
-      provider: 'aws-s3',
-      providerOptions: {
-        accessKeyId: env('AKIA35KCLFHBP3JCOI5F'),
-        secretAccessKey: env('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'),
-        region: env('us-west-1'),
-        params: {
-          Bucket: env('image-b354'),
-        },
+  // ...
+  upload: {
+    provider: 'aws-s3',
+    providerOptions: {
+      accessKeyId: env('AWS_ACCESS_KEY_ID','AKIA35KCLFHBP3JCOI5F'),
+      secretAccessKey: env('AWS_ACCESS_SECRET', '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'),
+      region: env('AWS_REGION','us-west-1'),
+      params: {
+        Bucket: env('AWS_BUCKET','image-b354'),
       },
     },
-    // ...
-  });
+  },
+  // ...
+});
+
