8afb4927a3d6c5b5dec93eb41f28aa75b05a66b1
Stephan Wong <80276544+skwong-code@users.noreply.github.com>
2021-11-29 15:02:53 -0700

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -3,11 +3,11 @@ module.exports = ({ env }) => ({
   upload: {
     provider: 'aws-s3',
     providerOptions: {
-      accessKeyId: env('AWS_ACCESS_KEY_ID','AKIA35KCLFHBP3JCOI5F'),
-      secretAccessKey: env('AWS_ACCESS_SECRET', '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'),
-      region: env('AWS_REGION','us-west-1'),
+      accessKeyId: env('AWS_ACCESS_KEY_ID'),
+      secretAccessKey: env('AWS_ACCESS_SECRET'),
+      region: env('AWS_REGION'),
       params: {
-        Bucket: env('AWS_BUCKET','image-b354'),
+        Bucket: env('AWS_BUCKET'),
       },
     },
   },
