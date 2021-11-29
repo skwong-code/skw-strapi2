310974f5002dd1eea9af075e1895a0c60f4b4b48
Stephan Wong <80276544+skwong-code@users.noreply.github.com>
2021-11-29 10:51:00 -0700

    update server.js

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url:'https://skw-strapi2.herokuapp.com',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'bf3a8fba16ed04a0e9b00f69820b2606'),
